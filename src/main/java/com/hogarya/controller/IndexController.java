b52db127f2af7f22dddbf55fc627129c5ac5b594
cent_ <cent_@DESKTOP-G2MQSSK>
2026-06-30 00:34:51 -0300

    Editar propiedades

--- src/main/java/com/hogarya/controller/IndexController.java
+++ src/main/java/com/hogarya/controller/IndexController.java
@@ -12,6 +12,6 @@ public class IndexController {
 	        return "index";
 	    }
 
-	 
+	
 
 }
--- src/main/java/com/hogarya/controller/PersonaController.java
+++ src/main/java/com/hogarya/controller/PersonaController.java
@@ -41,7 +41,7 @@ public class PersonaController {
 
         return "redirect:/personas";
     }
-    
+   
    
       
     
